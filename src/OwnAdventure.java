a9c120363ac80d5f538c05a2025cb1159a8b0187
student <student@ip-172-31-56-144.us-west-2.compute.internal>
2021-03-09 18:28:19 -0800

    Almost done with own  adventture

---
+++ src/OwnAdventure.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+import _01_else_if._4_are_you_happy.Main;
+
+public class OwnAdventure {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "I am goingto be walking tthrough this forest");
+	int answer  =  JOptionPane.showConfirmDialog(null, "Uh oh looks like this theres this guy here and he asked me if i want candy hey do u want candy");
+	if (answer==0) {
+		JOptionPane.showMessageDialog(null"wow he  didnt kidnap me he  just wan")
+}
+}
+}
