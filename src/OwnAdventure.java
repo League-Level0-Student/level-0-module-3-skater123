49ed994efb932d8ce8bf6ec57687f60e39764049
student <student@ip-172-31-54-30.us-west-2.compute.internal>
2021-03-16 18:24:51 -0700

    making progress with adventure

--- src/OwnAdventure.java
+++ src/OwnAdventure.java
@@ -3,11 +3,23 @@ import javax.swing.JOptionPane;
 import _01_else_if._4_are_you_happy.Main;
 
 public class OwnAdventure {
-public static void main(String[] args) {
-	JOptionPane.showMessageDialog(null, "I am goingto be walking tthrough this forest");
-	int answer  =  JOptionPane.showConfirmDialog(null, "Uh oh looks like this theres this guy here and he asked me if i want candy hey do u want candy");
-	if (answer==0) {
-		JOptionPane.showMessageDialog(null"wow he  didnt kidnap me he  just wan")
-}
-}
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "I am goingto be walking tthrough this forest");
+		int answer = JOptionPane.showConfirmDialog(null,
+				"Uh oh looks like this theres this guy here and " + "he asked me if i want candy hey do u want candy");
+		if (answer == 0) {
+			JOptionPane.showMessageDialog(null, "wow he  didnt kidnap me he  just wanted to give me candy");
+			
+			
+			
+			
+		} else if (answer == 1) {
+			JOptionPane.showMessageDialog(null, "you should take the candy");
+		} else {
+			JOptionPane.showMessageDialog(null, "hey you shouldn't cancel!");
+			
+			JOptionPane.showMessageDialog(null, "uh oh i think there is a ravenous fox");
+			
+		}
+	}
 }
