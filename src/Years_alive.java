0a148f3f806b935bcc9b852b7caf76a0a6a9975c
student <student@ip-172-31-57-21.us-west-2.compute.internal>
2021-03-23 18:05:49 -0700

    Finished Years Alive

---
+++ src/Years_alive.java
@@ -0,0 +1,13 @@
+
+public class Years_alive {
+	public static void main(String[] args) {
+		int YearsAlive = 2008;
+		for (int i = 0; i < 12; i++) {
+			//boorn 2008
+		
+			System.out.println(YearsAlive);
+			YearsAlive = YearsAlive+1;
+		}
+	}
+}
+
