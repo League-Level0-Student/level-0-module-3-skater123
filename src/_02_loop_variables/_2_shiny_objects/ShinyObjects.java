bd202f2743fc15de817704b379be40f33d911116
student <student@ip-172-31-53-69.us-west-2.compute.internal>
2021-04-13 18:08:27 -0700

    We did shiny objects

--- src/_02_loop_variables/_2_shiny_objects/ShinyObjects.java
+++ src/_02_loop_variables/_2_shiny_objects/ShinyObjects.java
@@ -5,6 +5,7 @@ package _02_loop_variables._2_shiny_objects;
 import java.io.File;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 
@@ -24,14 +25,17 @@ public class ShinyObjects {
 	void manyShinyObjects() {
 		
 		// 1. Call the method below to play Mister Zee
-		
+		playMisterZee();
 		
 		// 2. Ask the user how many shiny objects they want
-		
+		String buffer = JOptionPane.showInputDialog("how many shiny objects do you want?");
 		
 		// 3. Play the sound that many times
-
-
+		int num_objects = Integer.parseInt(buffer);
+		
+		for (int i = 0; i <num_objects; i++) {
+			playMisterZee(); 
+		}
 	}
 
 	/********************   Use this method. DON'T CHANGE THE CODE BELOW  ***********************/
---
+++ src/_02_loop_variables/_2_shiny_objects/test.java
@@ -0,0 +1,42 @@
+package _02_loop_variables._2_shiny_objects;
+
+public class test {
+	public static void main (String args[]) {
+		test tester = new test();
+		
+		for (int i = 0; i < 10; i++) {
+			tester.printMyName();
+		}
+		
+		tester.printMyName(); 
+	}
+	
+	public void printMyName() {
+		System.out.println("I am Alex Mendoza");
+		System.out.println("I am 12 years old");
+		System.out.println("I like to skateboard");
+		System.out.println("I like carne asada tacos");
+		System.out.println("I am Alex Mendoza");
+		System.out.println("I am 12 years old");
+		System.out.println("I like to skateboard");
+		System.out.println("I like carne asada tacos");
+		System.out.println("I am Alex Mendoza");
+		System.out.println("I am 12 years old");
+		System.out.println("I like to skateboard");
+		System.out.println("I like carne asada tacos");
+		System.out.println("I am Alex Mendoza");
+		System.out.println("I am 12 years old");
+		System.out.println("I like to skateboard");
+		System.out.println("I like carne asada tacos");
+		System.out.println("I am Alex Mendoza");
+		System.out.println("I am 12 years old");
+		System.out.println("I like to skateboard");
+		System.out.println("I like carne asada tacos");
+		System.out.println("I am Alex Mendoza");
+		System.out.println("I am 12 years old");
+		System.out.println("I like to skateboard");
+		System.out.println("I like carne asada tacos");
+	}
+}
+
+
