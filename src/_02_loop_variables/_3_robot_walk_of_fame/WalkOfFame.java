73f15b44bd11f691538877cc354cb8f724c98292
student <student@ip-172-31-54-242.us-west-2.compute.internal>
2021-05-04 18:00:28 -0700

    Fiinished Walk of FAme

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -15,13 +15,38 @@ rob.setX(100);
 		// 2. Make the robot draw a star shape. Hint: angle=144.
 rob.penDown();
 rob.setPenColor(Color.BLUE);
+rob.setSpeed(500);
 		// 3. Set the length of each line in the star to 30.
-rob.turn(45);
-rob.move(150);
-rob.turn(-90);
-rob.move(150);
-rob.turn(131);
-rob.move(150);
+for (int i = 0; i < 5; i++) {
+	rob.move(30);
+	rob.turn(144);
+	
+	
+}
+rob.hide();
+//rob.setSpeed(99);
+//rob.turn(45);
+//rob.move(150);
+//rob.turn(-90);
+//rob.move(150);
+//rob.turn(131);
+//rob.move(150);
+//rob.turn(-90);
+//rob.move(70);
+//rob.turn(140);
+//rob.move(70);
+//rob.turn(50);
+//rob.turn(-90);
+//rob.move(150);
+//rob.turn(120);
+//rob.move(150);
+//rob.turn(-80);
+//rob.move(120);
+//rob.turn(150);
+//rob.move(120);
+//rob.turn(-40);
+//rob.move(1920);
+
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
