c3a8e2034a4c883b6bd0cbe5132fa5323569b465
student <student@ip-172-31-62-93.us-west-2.compute.internal>
2021-08-31 17:31:58 -0700

    Finished Fizz Buzz

--- src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
+++ src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
@@ -23,6 +23,12 @@ package _03_modulo._4_fizz_buzz;
 public class FizzBuzz {
 public static void main(String[] args) {
 	 for (int i = 1; i <21; i++) {
+		 if (i%15==0) {
+			
+			 System.out.println("fizzbuzz");
+			 
+		 }
+		 else
 		 if (i%3==0)
 		
 			 System.out.println("fizz");
